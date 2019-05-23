34445c2150908a1b6693ad13c4320796ad67bb50
halky <yuyami.stars@gmail.com>
2019-05-23 22:32:29 +0900

    For depoy

--- src/ts/server-side-data-manager.ts
+++ src/ts/server-side-data-manager.ts
@@ -7,7 +7,7 @@ import Game from './game';
 export default class ServerSideDataManager {
   public static async getGameList(): Promise<Game[]> {
     return new Promise((resolve, reject) => {
-      const url = 'http://' + path.join(location.host, 'games.json');
+      const url = 'http://' + path.join(location.host, '2019/games.json');
       axios.default.get(url).then((res) => {
         resolve(res.data as Game[]);
       }).catch((error) => {
